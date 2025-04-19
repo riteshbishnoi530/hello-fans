c3eba6bf03965f4de62111d0f7377fafc624266e
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-04-19 12:17:18 +0530

    progress bar

---
+++ src/utils/helper.js
@@ -0,0 +1,11 @@
+export const freeRates = {
+    Top: { Top: 4.5, Medium: 3.5, Low: 1.5 },
+    Medium: { Top: 3.5, Medium: 2.5, Low: 1.0 },
+    Low: { Top: 2.0, Medium: 1.0, Low: 0.5 },
+  };
+  
+  export const paidRates = {
+    Top: { Top: 21.0, Medium: 18.0, Low: 12.0 },
+    Medium: { Top: 18.0, Medium: 15.0, Low: 9.0 },
+    Low: { Top: 12.0, Medium: 9.0, Low: 5.0 },
+  };
